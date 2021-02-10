12ebc8033ccd6ecc2b9101147b3251717cc074a1
leomiranda-tc <leonardo.miranda@tradersclub.com.br>
2021-02-09 21:25:26 -0300

    correções no script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,4 @@ rm -rf ./lib
 mkdir src/components
 echo "export default {}" > src/components/index.js
 tsc && cp ./src/svg/*.svg ./lib/svg
-npx @svgr/cli src/svg --config-file svgr.config.js -d src/components
 node scripts/generate.js
